161da82ed6c1dd32e41b93d718a01a4ca101b43a
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-03-07 18:23:30 +0100

    added coupled_simulation module

---
+++ coupled_simulation/__init__.py
@@ -0,0 +1,9 @@
+"""
+.. moduleauthor:: Francesco Witte <francesco.witte@hs-flensburg.de>
+
+"""
+__version__ = '0.0.1 dev'
+
+from coupled_simulation import coupling as cp
+from coupled_simulation import powerplant as pp
+from coupled_simulation import geostorage as gs

e6014bf05de2a99d4913222df87f8cf808da412f
Jens-Olaf Delfs <jod@gpi.uni-kiel.de>
2019-06-28 17:17:53 +0200

    coded coupling loop and data exchange

---
+++ coupled_simulation/properties.py
@@ -0,0 +1,25 @@
+import os
+import datetime
+from logging import info
+from json import load
+
+
+class Properties:
+    def __init__(self, path):
+        """
+        - store data from json file
+        :param path: (str) path to *.main_ctrl.json
+        """
+
+        with open(path) as file:
+            self.__dict__.update(load(file, parse_int=int, parse_float=float))
+
+        # self.auto_eval_output = True if self.eval_output == "True" else False
+        str_tmp = path.strip('.main_ctrl.json')
+
+        self.scenario = os.path.basename(str_tmp)
+        self.working_dir = os.path.dirname(str_tmp) + '/'  # to remove
+        self.t_start = datetime.datetime.strptime(self.t_start, '%Y-%m-%d %H:%M:%S')
+
+        info('Read inputile {}.main_ctrl.json\n    in {}'.format(self.scenario, self.working_dir))
+

76b7536480e541675859a02959c9bd3d63b43c63
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-10-14 16:26:39 +0200

    include heating system temperature levels

--- testdata/prepare_input_timeseries.py
+++ testdata/prepare_input_timeseries.py
@@ -2,12 +2,15 @@ import pandas as pd
 import sys
 
 ts = pd.read_csv(sys.argv[1])
+
+temperature = pd.read_csv(sys.argv[2], decimal=',', sep=';')
+
 ts['heat_target'] = ts['heat-storage'].diff() * 1e6
 ts['heat_target'] = ts['heat_target'].apply(
         lambda x: x / 1.1905 if x < 0 else x / 0.84)
 ts.loc[0, 'heat_target'] = 0
-ts['temperature_feed'] = 90
-ts['temperature_return'] = 60
+ts['temperature_feed'] = temperature['Temperatur VL']
+ts['temperature_return'] = temperature['Temperatur RL']
 ts['pressure_feed'] = 9.5
 ts['pressure_return'] = 10
 ts = ts.drop(columns=['heat-storage'])

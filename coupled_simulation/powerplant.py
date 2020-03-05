ca2379756707ba894aab33b8c6e5f0041b527e5d
Francesco Witte <francesco.witte@hs-flensburg.de>
2020-03-05 17:16:00 +0100

    Add discharging limitation logic

--- coupled_simulation/powerplant.py
+++ coupled_simulation/powerplant.py
@@ -223,6 +223,83 @@ def calc_interface_params(ppinfo, T_ff_sys, T_rf_sys, T_rf_sto, Q, mode):
         raise ValueError(msg)
 
 
+def calc_interface_params_limitation(ppinfo, T_ff_sys, T_rf_sys, T_rf_sto, m, mode):
+    """
+
+    Parameters
+    ----------
+    ppinfo : dict
+        Dictionary with information on TESPy power plant models.
+
+    T_ff_sys : float
+        Heating system feed flow temperature (to heating system).
+
+    T_rf_sys : float
+        Heating system return flow temperature (from heating system).
+
+    T_rf_sto : float
+        Storage return flow temperature (from storage).
+
+    m : float
+        Mass flow.
+
+    mode : str
+        Charging or discharging mode.
+
+    Returns
+    -------
+    IF_DATA : list
+        List of exchange parameters.
+    """
+
+    if mode == 'charging':
+
+        # read interface model information
+        charge = ppinfo['charge']['name']
+        ttd_restriction = ppinfo['charge']['restricted']
+        plant = ppinfo['power_plant_models'][charge]['plant']
+
+        # check for minimum heat transfer
+        Q_min = plant.model_data['Q_min'] * plant.model_data['Q_design']
+        if Q < Q_min:
+            # storage interface plant operation impossible
+            msg = ('Target heat extraction rate of ' + str(round(Q, 0)) + ' W '
+                   'below minimum possible heat extraction rate ofF ' +
+                   str(round(Q_min, 0)) + ' W. '
+                   'Interface operation impossible.')
+            logging.warning(msg)
+            T_ff_sto = T_rf_sto
+            return 0, 0, 0, 0, T_ff_sto, T_rf_sto, 0, False
+
+        else:
+
+            # temperature restriction for storage feed flow temperature
+            ttd = ppinfo['charge']['ttd_min']
+            T_ff_sto_max = ppinfo['charge']['T_ff_sto_max']
+
+            # system feed flow temperature as temperature at interface
+            # inlet
+            IF_data = sim_IF_charge(
+                plant, T_ff_sys, T_rf_sto, Q, ttd, T_ff_sto_max)
+
+        return IF_data
+
+    elif mode == 'discharging':
+
+        # read interface model information
+        discharge = ppinfo['discharge']['name']
+        plant = ppinfo['power_plant_models'][discharge]['plant']
+        IF_data = sim_IF_discharge_limitation(plant, T_ff_sys, T_rf_sys, T_rf_sto, m)
+        return IF_data
+
+    else:
+        msg = ('Mode for interface parameter calculation at '
+               'coupled_simulation.powerplant must be \'charging\' or '
+               '\'discharging\'.')
+        logging.error(msg)
+        raise ValueError(msg)
+
+
 def sim_IF_discharge(plant, T_ff_sys, T_rf_sys, T_rf_sto, Q):
     """
     Calculate the operation of the interface plant (e. g. heat exchanger or
@@ -480,6 +557,127 @@ def sim_IF_charge(plant, T_rf_sys, T_rf_sto, Q, ttd, T_ff_sto_max):
         elif m < m_min:
             msg = ('Shutting off plant due to mass flow restriction in '
                    'injection plant: mass flow: ' + str(round(m, 2)) +
+                   'kg/s; minimum mass flow: ' + str(round(m_min, 2)) +
+                    'kg/s.')
+            logging.warning(msg)
+            return 0, 0, 0, 0, 0, T_rf_sto, 0, False
+
+        conn.set_attr(m=np.nan)
+
+    # storage interface temperatures
+    T_ff_sys = model.imp_conns[plant.model_data['ff_sys']].T.val
+    T_ff_sto = model.imp_conns[plant.model_data['ff_sto']].T.val
+    T_rf_sto = model.imp_conns[plant.model_data['rf_sto']].T.val
+
+    # storage mass flow
+    m_sto = model.imp_conns[plant.model_data['ff_sto']].m.val
+
+    # interface transferred energy params
+    Q_sto = model.imp_busses[plant.model_data['heat_bus_sto']].P.val
+    Q_sys = model.imp_busses[plant.model_data['heat_bus_sys']].P.val
+    P_IF = model.imp_busses[plant.model_data['power_bus']].P.val
+    TI_IF = model.imp_busses[plant.model_data['ti_bus']].P.val
+
+    return Q_sto, Q_sys, P_IF, TI_IF, T_ff_sto, T_rf_sto, m_sto, False
+
+
+def sim_IF_discharge(plant, T_ff_sys, T_rf_sys, T_rf_sto, m):
+    """
+    Calculate the operation of the interface plant (e. g. heat exchanger or
+    heat pump) without temperature restirctions.
+
+    Parameters
+    ----------
+    plant : coupled_simulation.powerplant.model
+        Power plant model object, holding power plant information.
+
+    T_ff_sys : float
+        Heating system feed flow temperature (to heating system).
+
+    T_rf_sys : float
+        Heating system return flow temperature (to interface).
+
+    T_rf_sto : float
+        Storage return flow temperature (from storage).
+
+    Q : float
+        Value of heat flow.
+
+    Returns
+    -------
+    Q_sto : float
+        Heat transferred from storage.
+
+    Q_sys : float
+        Heat transferred to system.
+
+    P_IF : float
+        Power input/output of storage interface.
+
+    TI_IF : float
+        Thermal input of storage interface.
+
+    T_ff_sto : float
+        Storage feed flow temperature (to storage).
+
+    T_rf_sto : float
+        Storage return flow temperature (from storage).
+
+    mass_flow : float
+        District heating water mass flow through the plant.
+
+    err : bool
+        Indicates whether an error occurred in calculation.
+    """
+    model = plant.instance
+
+    # specify system parameters
+    model.imp_busses[plant.model_data['heat_bus_sys']].set_attr(P=np.nan)
+    model.imp_conns[plant.model_data['rf_sys']].set_attr(T=T_rf_sys)
+    model.imp_conns[plant.model_data['rf_sto']].set_attr(T=T_rf_sto, m=m)
+    model.imp_conns[plant.model_data['ff_sto']].set_attr(T=np.nan)
+    model.imp_conns[plant.model_data['ff_sys']].set_attr(
+            m=np.nan, T=T_ff_sys, design=[])
+
+    # solving
+    try:
+        try:
+            model.solve('offdesign', design_path=design)
+            if model.lin_dep or model.res[-1] > 1e-3:
+                raise TESPyNetworkError
+
+        except (TESPyNetworkError, ValueError):
+            model.solve('offdesign', design_path=design, init_path=design)
+            if model.lin_dep or model.res[-1] > 1e-3:
+                raise TESPyNetworkError
+
+    except (TESPyNetworkError, ValueError):
+        model.lin_dep = True
+
+    if model.lin_dep or model.res[-1] > 1e-3:
+        return 0, 0, 0, 0, 0, T_rf_sto, 0, True
+
+    for conn_id, limits in plant.model_data['limiting_mass_flow'].items():
+        conn = model.imp_conns[conn_id]
+        m_max = conn.m.design * limits[1]
+        m_min = conn.m.design * limits[0]
+        m = conn.m.val_SI
+
+        if m > m_max:
+            model.imp_conns[plant.model_data['rf_sto']].set_attr(m=np.nan)
+            m_range = np.linspace(m_max, m, num=3, endpoint=False)
+            for m_val in m_range[::-1]:
+                conn.set_attr(m=m_val)
+                model.solve('offdesign', design_path=design)
+            msg = ('Limiting heat flow due to mass flow restriction in '
+                   'extraction plant: mass flow: ' + str(round(m, 2)) +
+                   'kg/s; maximum mass flow: ' + str(round(m_max, 2)) +
+                    'kg/s.')
+            logging.warning(msg)
+
+        elif m < m_min:
+            msg = ('Shutting off plant due to mass flow restriction in '
+                   'extraction plant: mass flow: ' + str(round(m, 2)) +
                    'kg/s; minimum mass flow: ' + str(round(m_min, 2)) +
                     'kg/s.')
             logging.warning(msg)
